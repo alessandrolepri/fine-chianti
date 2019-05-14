1b9d10760aa289ad16f02acbd7dd3d6e8a722fe6
Alessandro Lepri <alex.lepri84@gmail.com>
2019-05-14 12:31:34 +0100

    Added Tignanello

--- src/components/ShowPage.js
+++ src/components/ShowPage.js
@@ -4,11 +4,29 @@ import React from 'react'
 const ShowPage = () => {
   return (
     <section className="section">
-      <div className="container">
-        <h1 className="title is-1">Tignanello</h1>
+      <div className="container red">
+        <h1 className="title">Tignanello</h1>
+        <h2 className="subtitleis">Location: Florence, Italy</h2>
+        <h2 className="web"> <a href="https://www.antinori.it/it/"  target="_blank" rel="noreferrer noopener"> Visit Official Website</a></h2>
+        <hr />
+
+        <div className="columns">
+          <div className="column">
+            <figure className="image-wine">
+              <img src="https://www.garyswine.com/images/sites/garyswine/labels/marchesi-antinori-antinori-tignanello_1.jpg" alt='Tignanello' />
+            </figure>
+          </div>
+
+          <div className="column">
+            <h4 className="tasting"><strong>Tasting Notes:</strong></h4>
+            <p> Tignanello was the first Sangiovese to be aged in barriques, the first contemporary red wine blended with untraditional varieties (specifically Cabernet) and one of the first red wines in the Chianti Classico region that didn’t use white grapes. Tignanello is a milestone. It’s produced with a selection of Sangiovese, Cabernet Sauvignon and Cabernet Franc. </p>
+            <img className='map'  src="https://ubriaco.files.wordpress.com/2011/02/chianti-map.jpg" />
+            <hr />
+          </div>
+        </div>
+
       </div>
     </section>
-
   )
 }
 
