c50d052fa09d4ed1a034b85195571238824182aa
Alessandro Lepri <alex.lepri84@gmail.com>
2019-05-14 10:49:26 +0100

    packages installed

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+// import './style.scss'
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <main>
+        <h1> Hello </h1>
+      </main>
+    )
+  }
+}
+
+ReactDOM.render( <App />, document.getElementById('root'))
